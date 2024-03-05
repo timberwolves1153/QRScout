b1c8c22863473da9de27ba3339293936be1de929
still <git@stillconfusing.net>
2024-03-04 19:50:15 -0500

    added animation to more buttons

--- src/components/Sections/CommitAndResetSection/CommitButton.tsx
+++ src/components/Sections/CommitAndResetSection/CommitButton.tsx
@@ -1,15 +1,19 @@
+import { useState } from "preact/hooks";
+
 export type CommitButtonProps = {
   onClick: () => void;
   disabled: boolean;
 };
 
 export function CommitButton(props: CommitButtonProps) {
+  const [effect, setEffect] = useState(false);
   return (
     <button
-      className="on-click:animate-pulse focus:shadow-outline mx-2 rounded bg-gray-700 py-6 px-6 m-2 font-bold uppercase text-white hover:bg-gray-700 focus:shadow-lg focus:outline-none disabled:bg-gray-300 dark:bg-primary"
+      className={ `${effect && "animate-wiggle" } on-click:animate-pulse focus:shadow-outline mx-2 rounded bg-gray-700 py-6 px-6 m-2 font-bold uppercase text-white hover:bg-gray-700 focus:shadow-lg focus:outline-none disabled:bg-gray-300 dark:bg-primary`}
       type="button"
-      onClick={props.onClick}
+      onClick={() => {setEffect(true); props.onClick()}}
       disabled={props.disabled}
+      onAnimationEnd={() => setEffect(false)}
     >
       Commit
     </button>
--- src/components/Sections/CommitAndResetSection/LoadButton.tsx
+++ src/components/Sections/CommitAndResetSection/LoadButton.tsx
@@ -1,15 +1,19 @@
+import { useState } from "preact/hooks";
+
 export type LoadButtonProps = {
   disabled?: boolean;
   onClick: () => void;
 };
 
 export function LoadButton(props: LoadButtonProps) {
+  const [effect, setEffect] = useState(false);
   return (
     <button
-      className="focus:shadow-outline mx-2 rounded bg-gray-700 py-6 px-6 m-2 font-bold uppercase text-white hover:bg-gray-700 focus:shadow-lg focus:outline-none disabled:bg-gray-300 dark:bg-primary"
+      className={ `${effect && "animate-wiggle" } focus:shadow-outline mx-2 rounded bg-gray-700 py-6 px-6 m-2 font-bold uppercase text-white hover:bg-gray-700 focus:shadow-lg focus:outline-none disabled:bg-gray-300 dark:bg-primary`}
       type="button"
-      onClick={props.onClick}
+      onClick={() => {setEffect(true); props.onClick()}}
       disabled={props.disabled}
+      onAnimationEnd={() => setEffect(false)}
     >
       Load QRs from Local Storage
     </button>
